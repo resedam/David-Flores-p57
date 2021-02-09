7fae8d5be85061840db4ccf3d2fc5e1cadbc529b
resedam <56598649+resedam@users.noreply.github.com>
2021-02-09 00:17:45 -0500

    Add files via upload

---
+++ LISTAS.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 14:20:28 2020
+
+@author: david
+"""
+
+lista=["R1",5.5,6,True,"R2"]
+print(lista[1])
+print(lista[-1])
+print(lista[-5])
+del lista[4]
+print(lista)
+lista[1]="R3"
+print(lista)
