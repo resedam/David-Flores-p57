6bc3c21a93c14c6ca9480d2bf4845b726781f3f2
resedam <56598649+resedam@users.noreply.github.com>
2021-02-09 00:32:27 -0500

    Add files via upload

---
+++ año bisisesto.py
@@ -0,0 +1,12 @@
+a=int(input(" ingrese el año"))
+if(a%4==0 and a % 100 != 0 or a % 400 == 0):
+
+    print("el año es bisiesto")
+else:
+      print("el año no es bisiesto")
+
+    
+    
+        
+    
+    
